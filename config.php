ba37481bb4e35e9782ab6edf05d3289170113f2d
fawadiqbal <fwd82@live.com>
2021-08-01 00:48:29 +0500

    initial commit

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+
+	define('DB_HOST', $_SERVER['SERVER_NAME']);
+	define('DB_NAME', 'gsm_api');
+	define('DB_USER', 'root');
+	define('DB_PWD', '');
+
+	define('API_ADDRESS', $_SERVER['SERVER_NAME'].'/GSM_API/');
+
+
+/*
+
+	[Fetch New Messages]
+		http://localhost/GSM_API/gsm_api.php?action=fetch_new
+
+	[Fetch All Messages]
+		http://localhost/GSM_API/gsm_api.php?action=fetch_all
+
+	[Send Message - Insert Data to DB]
+		http://localhost/GSM_API/gsm_api.php?action=insert&name=FAWAD&mobile=03001234567&message=Welcome&status=0 
+
+	[Update Message Status - Insert Data to DB]
+		http://localhost/GSM_API/gsm_api.php?action=update&id=1&status=0 
+
+
+	[Delete Message / Del Record from DB]
+		http://localhost/GSM_API/gsm_api.php?action=delete&id=2
+
+*/
+
+
