808b46ef3ade7063268d5e0177436048dcb2f591
Fawad Iqbal <fwd82@live.com>
2021-08-04 12:38:24 +0500

    Update config.php

--- config.php
+++ config.php
@@ -5,28 +5,26 @@
 	define('DB_USER', 'root');
 	define('DB_PWD', '');
 
-	define('API_ADDRESS', $_SERVER['SERVER_NAME'].'/GSM_API/');
+	define('API_ADDRESS', $_SERVER['SERVER_NAME'].'/api/'); // api | GSM_API is name of your directory in public_html 
 
 
 /*
-
         You can set your directory name as "api" or "GSM_API"
 
 	[Fetch New Messages]
-		http://localhost/GSM_API/gsm_api.php?action=fetch_new
+		http://localhost/api/gsm_api.php?action=fetch_new
 
 	[Fetch All Messages]
-		http://localhost/GSM_API/gsm_api.php?action=fetch_all
+		http://localhost/api/gsm_api.php?action=fetch_all
 
 	[Send Message - Insert Data to DB]
-		http://localhost/GSM_API/gsm_api.php?action=insert&name=FAWAD&mobile=03001234567&message=Welcome&status=0 
+		http://localhost/api/gsm_api.php?action=insert&name=FAWAD&mobile=03001234567&message=Welcome&status=0 
 
 	[Update Message Status - Insert Data to DB]
-		http://localhost/GSM_API/gsm_api.php?action=update&id=1&status=0 
-
+		http://localhost/api/gsm_api.php?action=update&id=1&status=0 
 
 	[Delete Message / Del Record from DB]
-		http://localhost/GSM_API/gsm_api.php?action=delete&id=2
+	http://localhost/api/gsm_api.php?action=delete&id=2
 
 */
 
